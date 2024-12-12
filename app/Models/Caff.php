da027426bf8a9931f0df91a74798ecb93233c2ab
Pascal <20220269@webmail.univ-corse.fr>
2024-12-12 20:46:54 +0100

    Add old app version

---
+++ app/Models/Caff.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Support\Facades\Hash;
+
+class Caff extends Authenticatable implements AuthenticatableContract
+{
+    use Notifiable;
+
+    // Spécifie le nom de la table associée au modèle
+    protected $table = 'caff';
+
+    protected $fillable = [
+        'nom',
+        'prenom',
+        'ville',
+        'code_postal',
+        'email',
+        'telephone',
+        'username',
+        'password',
+        'manager_id',
+        'grade_id',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    // Relation pour le manager
+    public function manager()
+    {
+        return $this->belongsTo(Caff::class, 'manager_id');
+    }
+
+    // Relation pour les subordonnés
+    public function subordinates()
+    {
+        return $this->hasMany(Caff::class, 'manager_id');
+    }
+
+    // Relation pour le grade
+    public function grade()
+    {
+        return $this->belongsTo(Grade::class);
+    }
+
+    // Mutateur pour hacher le mot de passe
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = Hash::make($value);
+    }
+}
--- config/auth.php
+++ config/auth.php
@@ -38,7 +38,7 @@ return [
     'guards' => [
         'web' => [
             'driver' => 'session',
-            'provider' => 'users',
+            'provider' => 'caff',
         ],
     ],
 
@@ -60,15 +60,10 @@ return [
     */
 
     'providers' => [
-        'users' => [
+        'caff' => [
             'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', App\Models\User::class),
+            'model' => App\Models\Caff::class,
         ],
-
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
     ],
 
     /*
@@ -92,7 +87,7 @@ return [
 
     'passwords' => [
         'users' => [
-            'provider' => 'users',
+            'provider' => 'caff',
             'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
             'expire' => 60,
             'throttle' => 60,
