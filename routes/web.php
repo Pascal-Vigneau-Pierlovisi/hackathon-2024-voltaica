4dde76b94b376d59eae14aa0fcbc54753c8abaa6
Pascal <20220269@webmail.univ-corse.fr>
2024-12-12 21:07:33 +0100

    Ajout de la route pour voir la page de login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\ShowLoginController;
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/login', ShowLoginController::class)->name('login');
