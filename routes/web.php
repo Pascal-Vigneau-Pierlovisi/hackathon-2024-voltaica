9eb85003a071dc11d46236b51da2bf76819afbfb
Pascal <20220269@webmail.univ-corse.fr>
2024-12-12 21:37:25 +0100

    Ajout de la route pour se login

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\ShowLoginController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 
 Route::get('/login', ShowLoginController::class)->name('login');
+
+Route::post('/login', LoginController::class);
