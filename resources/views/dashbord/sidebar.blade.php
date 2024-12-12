543baaa7dd09f3a8be545ddc0a942fa33dd50591
ayoub oubakki <ayoubouba>
2024-12-12 22:28:14 +0100

    dashbord v1

---
+++ resources/views/dashbord/sidebar.blade.php
@@ -0,0 +1,51 @@
+      <!-- ========== Left Sidebar Start ========== -->
+            <div class="left side-menu">
+                <div class="slimscroll-menu" id="remove-scroll">
+
+                    <!--- Sidemenu -->
+                    <div id="sidebar-menu">
+                        
+                        <!-- Left Menu Start -->
+                        <ul class="metismenu" id="side-menu">
+
+                           
+                                <li class="menu-title">Index</li>
+                                
+                                <li class="">
+                                    <a href="/home" class="waves-effect">
+                                        <i class="ti-home"></i> <span> Dashboard </span>
+                                    </a>
+                                </li>
+                                <li class="">
+                                    <a href="/orders" class="waves-effect">
+                                        <i class="ti-calendar"></i> <span> Orders</span>
+                                    </a>
+                                </li>
+                            <li class="menu-title">Management</li>
+                          
+                          
+                           
+                            <li class="">
+                                <a href="" >
+                                    <i class="dripicons-backspace"></i> <span> Logout </span>
+                                </a>
+                                <!-- <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form> -->
+                            </li>
+                            
+                          
+                            
+                           
+
+                        </ul>
+
+                    </div>
+                    <!-- Sidebar -->
+                    <div class="clearfix"></div>
+
+                </div>
+                <!-- Sidebar -left -->
+
+            </div>
+            <!-- Left Sidebar End -->
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\ShowLoginController;
 
 Route::get('/login', ShowLoginController::class)->name('login');
+
